a72209b350be6613df19f596527799958b237ee7
Fukuo Kadota <app.fk91@gmail.com>
2015-04-18 02:34:41 +0900

    Fix bug

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -1,17 +1,15 @@
 ActiveAdmin.register Author do
   permit_params :name,
-    posts_attributes: [:title, :body]
+    { posts_attributes: [:title, :body, :_destroy, :id]}
 
   form(html: { multipart: true }) do |f|
     f.inputs 'Author details' do
       f.input :name
     end
 
-    f.inputs 'Posts' do
-      f.has_many :posts, heading: 'Posts', allow_destroy: true, new_record: true do |post|
-        post.input :title
-        post.input :body
-      end
+    f.has_many :posts, heading: 'Posts', allow_destroy: true, new_record: true do |post|
+      post.input :title
+      post.input :body
     end
 
     f.actions
