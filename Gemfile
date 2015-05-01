473fbbe71d306cba5cdb6f7a757cc2ac9cfaf5b0
Fukuo Kadota <app.fk91@gmail.com>
2015-05-01 18:28:04 +0900

    Add gem configurable_engine

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'activeadmin'
+gem 'configurable_engine'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417090850) do
+ActiveRecord::Schema.define(version: 20150501084959) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20150417090850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "configurables", force: :cascade do |t|
+    t.string   "name"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "configurables", ["name"], name: "index_configurables_on_name"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
